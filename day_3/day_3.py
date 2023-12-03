ce08dd88f36961225ec4382ef5d6ba56d2ae2a7c
Niklas Schulte <schultenik@gmail.com>
2023-12-03 16:45:26 +0100

    day_3_1 complete

--- day_3/day_3.py
+++ day_3/day_3.py
@@ -1,14 +1,59 @@
 import re
 
-with open("/Users/nschulte/git/adventOfCode2023/day_3/input", "r") as f:
+expression = re.compile("[^A-Za-z0-9.\n]")
+
+with (open("./input", "r") as f):
     lines = f.readlines()
 
     total = 0
-    for line in lines:
-        # (start, end, number)
+    for line_number, line in enumerate(lines):
+        # find all numbers and indices in line (start, end, number)
         numbers_in_line = [(m.start(0), m.end(0), m.group(0)) for m in re.finditer(r'\d+', line)]
-        for start, end, number in numbers_in_line:
-            print(line[start-1], line[end+1])
+        numbers_found_in_line = []
 
-        print(numbers_in_line, line)
-        break
+        for start, end, number in numbers_in_line:  # iterate over each number in the current line
+            adjacent_chars = ""
+
+            for index_in_line in range(start, end):  # iterate over all digits
+
+                try:
+                    a1 = lines[line_number - 1][index_in_line - 1]
+                except:
+                    pass
+                try:
+                    a2 = lines[line_number - 1][index_in_line]
+                except:
+                    pass
+                try:
+                    a3 = lines[line_number - 1][index_in_line + 1]
+                except:
+                    pass
+                try:
+                    b1 = line[index_in_line - 1]
+                except:
+                    pass
+                try:
+                    b2 = line[index_in_line + 1]
+                except:
+                    pass
+                try:
+                    c1 = lines[line_number + 1][index_in_line - 1]
+                except:
+                    pass
+                try:
+                    c2 = lines[line_number + 1][index_in_line]
+                except:
+                    pass
+                try:
+                    c3 = lines[line_number + 1][index_in_line + 1]
+                except:
+                    pass
+                adjacent_chars = adjacent_chars + a1 + a2 + a3 + b1 + b2 + c1 + c2 + c3
+
+            if expression.findall(adjacent_chars):
+                numbers_found_in_line.append((number, adjacent_chars))
+                total += int(number)
+
+        print(line_number, numbers_found_in_line)
+
+print(total)
